d8cb42621542e3a3d60e7680895e36d34e95d35c
Emil Renner Berthing <kernel@esmil.dk>
2017-03-06 19:58:49 +0100

    spi: imx: don't use DMA on imx50

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -948,6 +948,10 @@ static int spi_imx_sdma_init(struct device *dev, struct spi_imx_data *spi_imx,
 	if (of_machine_is_compatible("fsl,imx6dl"))
 		return 0;
 
+	/* use pio mode for i.mx50 chip */
+	if (of_machine_is_compatible("fsl,imx50"))
+		return 0;
+
 	spi_imx->wml = spi_imx_get_fifosize(spi_imx) / 2;
 
 	/* Prepare for TX DMA: */
