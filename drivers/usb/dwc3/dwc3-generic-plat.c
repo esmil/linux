79009738388a09939d03464a8fc2ba9f1546b66c
Junzhong Pan <junzhong.pan@spacemit.com>
2025-09-16 20:39:52 +0800

    usb: dwc3: generic: add support for spacemit,k1-dwc3
    SpacemiT DWC3 controller has vendor registers to support USB wakeup,
    support it according to compatible match.
    
    If reset is managed by DWC3 core, it will reset controller when USB
    wakeup is not enabled. Add a reset-on-resume property to control reset
    behavior of generic-plat driver during suspend, and it could be passed
    to xhci driver.
    
    Change-Id: Ia2c9b74ced9688181a5fa6e1910d66c83cf2accb
    Signed-off-by: Junzhong Pan <junzhong.pan@spacemit.com>

--- drivers/usb/dwc3/dwc3-generic-plat.c
+++ drivers/usb/dwc3/dwc3-generic-plat.c
@@ -10,17 +10,100 @@
 #include <linux/clk.h>
 #include <linux/platform_device.h>
 #include <linux/reset.h>
+#include <linux/pm_wakeirq.h>
 #include "glue.h"
 
+#ifdef CONFIG_SOC_SPACEMIT
+#define SPACEMIT_K1_LFPS_WAKE_STATUS BIT(29)
+#define SPACEMIT_K1_CDWS_WAKE_STATUS BIT(28)
+#define SPACEMIT_K1_ID_WAKE_STATUS BIT(27)
+#define SPACEMIT_K1_VBUS_WAKE_STATUS BIT(26)
+#define SPACEMIT_K1_LINS1_WAKE_STATUS BIT(25)
+#define SPACEMIT_K1_LINS0_WAKE_STATUS BIT(24)
+
+#define SPACEMIT_K1_CDWS_WAKE_CLEAR BIT(20)
+#define SPACEMIT_K1_ID_WAKE_CLEAR BIT(19)
+#define SPACEMIT_K1_VBUS_WAKE_CLEAR BIT(18)
+#define SPACEMIT_K1_LINS1_WAKE_CLEAR BIT(17)
+#define SPACEMIT_K1_LINS0_WAKE_CLEAR BIT(16)
+#define SPACEMIT_K1_LFPS_WAKE_CLEAR BIT(14)
+
+#define SPACEMIT_K1_WAKEUP_INT_MASK BIT(15)
+#define SPACEMIT_K1_LFPS_WAKE_MASK BIT(13)
+#define SPACEMIT_K1_CDWS_WAKE_MASK BIT(12)
+#define SPACEMIT_K1_ID_WAKE_MASK BIT(11)
+#define SPACEMIT_K1_VBUS_WAKE_MASK BIT(10)
+#define SPACEMIT_K1_LINS1_WAKE_MASK BIT(9)
+#define SPACEMIT_K1_LINS0_WAKE_MASK BIT(8)
+#endif
 struct dwc3_generic {
 	struct device		*dev;
 	struct dwc3		dwc;
 	struct clk_bulk_data	*clks;
 	int			num_clocks;
 	struct reset_control	*resets;
+#ifdef CONFIG_SOC_SPACEMIT
+	bool reset_on_resume;
+	void *priv;
+#endif
 };
 
 #define to_dwc3_generic(d) container_of((d), struct dwc3_generic, dwc)
+
+#ifdef CONFIG_SOC_SPACEMIT
+struct spacemit_k1_privdata {
+	void __iomem *wakeup_reg;
+	int wakeup_irq;
+};
+
+static void __maybe_unused
+spacemit_k1_enable_wakeup_irqs(struct dwc3_generic *dwc3g)
+{
+	struct spacemit_k1_privdata *priv = dwc3g->priv;
+	u32 reg;
+
+	reg = readl(priv->wakeup_reg);
+	reg |= (SPACEMIT_K1_LFPS_WAKE_MASK | SPACEMIT_K1_LINS0_WAKE_MASK |
+		SPACEMIT_K1_LINS1_WAKE_MASK);
+	writel(reg, priv->wakeup_reg);
+}
+
+static void spacemit_k1_disable_wakeup_irqs(struct dwc3_generic *dwc3g)
+{
+	struct spacemit_k1_privdata *priv = dwc3g->priv;
+	u32 reg;
+
+	reg = readl(priv->wakeup_reg);
+	reg &= ~(SPACEMIT_K1_LFPS_WAKE_MASK | SPACEMIT_K1_LINS0_WAKE_MASK |
+		 SPACEMIT_K1_LINS1_WAKE_MASK);
+	writel(reg, priv->wakeup_reg);
+}
+
+static void spacemit_k1_clear_wakeup_irqs(struct dwc3_generic *dwc3g)
+{
+	struct spacemit_k1_privdata *priv = dwc3g->priv;
+	u32 reg;
+
+	reg = readl(priv->wakeup_reg);
+	reg |= (SPACEMIT_K1_LFPS_WAKE_CLEAR | SPACEMIT_K1_LINS0_WAKE_CLEAR |
+		SPACEMIT_K1_LINS1_WAKE_CLEAR);
+	writel(reg, priv->wakeup_reg);
+}
+
+static irqreturn_t spacemit_k1_wakeup_interrupt(int irq, void *data)
+{
+	struct dwc3_generic *dwc3g = data;
+	struct spacemit_k1_privdata *priv = dwc3g->priv;
+	u32 reg;
+
+	reg = readl(priv->wakeup_reg);
+
+	spacemit_k1_disable_wakeup_irqs(dwc3g);
+	spacemit_k1_clear_wakeup_irqs(dwc3g);
+
+	return IRQ_HANDLED;
+}
+#endif
 
 static void dwc3_generic_reset_control_assert(void *data)
 {
@@ -66,9 +149,65 @@ static int dwc3_generic_probe(struct platform_device *pdev)
 	if (ret)
 		return ret;
 
-	ret = devm_clk_bulk_get_all_enabled(dwc3g->dev, &dwc3g->clks);
+	ret = devm_clk_bulk_get_all_enable(dwc3g->dev, &dwc3g->clks);
 	if (ret < 0)
 		return dev_err_probe(dev, ret, "failed to get clocks\n");
+
+#ifdef CONFIG_SOC_SPACEMIT
+	dwc3g->reset_on_resume =
+		device_property_read_bool(dev, "reset-on-resume");
+
+	if (of_device_is_compatible(dev->of_node, "spacemit,k1-dwc3")) {
+		if (device_property_read_bool(dev, "wakeup-source")) {
+			if (dwc3g->reset_on_resume)
+				return dev_err_probe(
+					dev, -EINVAL,
+					"cannot both reset and wakeup during suspend\n");
+
+			struct spacemit_k1_privdata *priv;
+			struct resource *wakeup_res;
+
+			priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+			if (!priv)
+				return -ENOMEM;
+
+			wakeup_res =
+				platform_get_resource(pdev, IORESOURCE_MEM, 1);
+			if (!res)
+				return dev_err_probe(
+					dev, -ENOMEM,
+					"cannot get wakeup reg resource\n");
+
+			priv->wakeup_reg =
+				devm_ioremap(dev, wakeup_res->start,
+					     resource_size(wakeup_res));
+			if (IS_ERR(priv->wakeup_reg))
+				return dev_err_probe(
+					dev, PTR_ERR(priv->wakeup_reg),
+					"Failed to get wakeup reg\n");
+
+			priv->wakeup_irq = platform_get_irq(pdev, 1);
+			if (priv->wakeup_irq < 0) {
+				dev_err(dev, "missing IRQ resource\n");
+				return -EINVAL;
+			}
+
+			dwc3g->priv = priv;
+			ret = devm_request_irq(dev, priv->wakeup_irq,
+					       spacemit_k1_wakeup_interrupt,
+					       IRQF_NO_SUSPEND, dev_name(dev),
+					       dwc3g);
+			if (ret) {
+				dev_err(dev,
+					"failed to request IRQ #%d --> %d\n",
+					priv->wakeup_irq, ret);
+				return ret;
+			}
+			device_init_wakeup(dev, true);
+			dev_pm_set_wake_irq(dev, priv->wakeup_irq);
+		}
+	}
+#endif
 
 	dwc3g->num_clocks = ret;
 	dwc3g->dwc.dev = dev;
@@ -101,6 +240,20 @@ static int dwc3_generic_suspend(struct device *dev)
 
 	clk_bulk_disable_unprepare(dwc3g->num_clocks, dwc3g->clks);
 
+#ifdef CONFIG_SOC_SPACEMIT
+	if (dwc3g->reset_on_resume) {
+		ret = reset_control_assert(dwc3g->resets);
+		if (ret)
+			return ret;
+	}
+
+	if (of_device_is_compatible(dev->of_node, "spacemit,k1-dwc3") &&
+	    device_may_wakeup(dev)) {
+		spacemit_k1_clear_wakeup_irqs(dwc3g);
+		spacemit_k1_enable_wakeup_irqs(dwc3g);
+	}
+#endif
+
 	return 0;
 }
 
@@ -109,6 +262,14 @@ static int dwc3_generic_resume(struct device *dev)
 	struct dwc3 *dwc = dev_get_drvdata(dev);
 	struct dwc3_generic *dwc3g = to_dwc3_generic(dwc);
 	int ret;
+
+#ifdef CONFIG_SOC_SPACEMIT
+	if (dwc3g->reset_on_resume) {
+		ret = reset_control_deassert(dwc3g->resets);
+		if (ret)
+			return ret;
+	}
+#endif
 
 	ret = clk_bulk_prepare_enable(dwc3g->num_clocks, dwc3g->clks);
 	if (ret)
