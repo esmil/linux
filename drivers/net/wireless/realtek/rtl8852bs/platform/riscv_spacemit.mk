3f29945bc2e243b6cc9c3bfdd3307e05aa7f32e4
wanlong <long.wan@spacemit.com>
2025-10-20 10:41:29 +0800

    wifi: rtl8852bs: Add support for realtek 8852bs Soc
    version: v1.19.17-248-g19f2c2a389.20250826
    
    Change-Id: I20e562240f3fca06ae0c87635579f9c2a0e6a675
    Signed-off-by: wanlong <long.wan@spacemit.com>

---
+++ drivers/net/wireless/realtek/rtl8852bs/platform/riscv_spacemit.mk
@@ -0,0 +1,20 @@
+ifeq ($(CONFIG_PLATFORM_SPACEMIT), y)
+EXTRA_CFLAGS += -DCONFIG_LITTLE_ENDIAN
+EXTRA_CFLAGS += -DCONFIG_PLATFORM_SPACEMIT
+EXTRA_CFLAGS += -DCONFIG_IOCTL_CFG80211 -DRTW_USE_CFG80211_STA_EVENT
+EXTRA_CFLAGS += -DCONFIG_RADIO_WORK
+#EXTRA_CFLAGS += -DCONFIG_CONCURRENT_MODE
+EXTRA_CFLAGS += -Wno-missing-prototypes -Wno-missing-declarations -Wno-old-style-declaration -Wno-empty-body
+ifeq ($(shell test $(CONFIG_RTW_ANDROID) -ge 11; echo $$?), 0)
+EXTRA_CFLAGS += -DCONFIG_IFACE_NUMBER=2
+#EXTRA_CFLAGS += -DCONFIG_SEL_P2P_IFACE=1
+endif
+
+ARCH := riscv
+CROSS_COMPILE=/home/wanlong/workspace/k2/output/toolchain/bin/riscv64-unknown-linux-gnu-
+KSRC=/home/wanlong/workspace/k2/linux-6.18
+
+ifeq ($(CONFIG_SDIO_HCI), y)
+_PLATFORM_FILES = platform/platform_spacemit_sdio.o
+endif
+endif
