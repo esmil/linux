5d35514ab98194e88a58a26387d26333f2dbe45d
Nell <xianbin.zhu@spacemit.com>
2026-01-01 18:00:04 +0800

    k3:thermal: add thermal driver
    Change-Id: I135af0e1f2563020674bfd130cf35bd384751496
    Signed-off-by: Nell <xianbin.zhu@spacemit.com>

---
+++ drivers/thermal/k3-thermal.c
@@ -0,0 +1,242 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <linux/cpufreq.h>
+#include <linux/delay.h>
+#include <linux/interrupt.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/io.h>
+#include <linux/of_device.h>
+#include <linux/thermal.h>
+#include <linux/reset.h>
+#include "thermal_hwmon.h"
+#include "thermal_core.h"
+#include "k3-thermal.h"
+
+#define MAX_SENSOR_NUMBER		8
+
+static int init_sensors(struct platform_device *pdev)
+{
+	int ret;
+	unsigned int val;
+	struct k3_thermal_sensor *s = platform_get_drvdata(pdev);
+
+	/* read the sensor range */
+	ret = of_property_read_u32_array(pdev->dev.of_node, "sensor_range", s->sr, 2);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "get sensor range error\n");
+		return ret;
+	}
+
+	if (s->sr[1] >= MAX_SENSOR_NUMBER) {
+		dev_err(&pdev->dev, "un-fitable sensor range\n");
+		return -EINVAL;
+	}
+
+	/* first: disable all the interrupts */
+        writel(0xffffffff, s->base + REG_TSEN_LITE_INT_CLR);
+        writel(0xffffffff, s->base + REG_TSEN_LITE_INT_ENB);
+
+        /* select clk div 26M/4 */
+        val = readl(s->base + REG_TSEN_LITE_CFG);
+        val &= ~BITS_D_CK_DIV_SEL;
+        val |= BITS_CK_DIV_SEL_DIV4;
+        writel(val, s->base + REG_TSEN_LITE_CFG);
+
+	return 0;
+}
+
+static void enable_sensors(struct platform_device *pdev)
+{
+	struct k3_thermal_sensor *s = platform_get_drvdata(pdev);
+
+	writel(readl(s->base + REG_TSEN_LITE_CFG) | BIT_TSEN_EN,
+			s->base + REG_TSEN_LITE_CFG);
+}
+
+static int k3_thermal_get_temp(struct thermal_zone_device *tz, int *temp)
+{
+	struct k3_thermal_sensor_desc *desc = (struct k3_thermal_sensor_desc *)tz->devdata;
+
+	/* select which sensor */
+	writel(desc->index, desc->base + REG_TSEN_LITE_CFG2);
+	*temp = readl(desc->base + REG_TSEN_LITE_TEMP_DATA);
+	*temp &= BITS_TEMP_DATA;
+	*temp /= TEMP_RAW_DATA_DIV;
+
+	*temp -= desc->temp_offset;
+
+	*temp *= 1000;
+
+	return 0;
+}
+
+/**
+ * static int k3_thermal_set_trips(struct thermal_zone_device *tz, int low, int high)
+ * {
+ *	int index;
+ *	unsigned int temp;
+ *	int over_thrsh = high;
+ *	int under_thrsh = low;
+ *	struct k3_thermal_sensor_desc *desc = (struct k3_thermal_sensor_desc *)tz->devdata;
+ *
+ *	index = desc - desc;
+ *	writel(index, desc->base + REG_TSEN_LITE_CFG2);
+ *
+ *	over_thrsh /= 1000;
+ *	over_thrsh += desc->temp_offset;
+ *	over_thrsh *= TEMP_RAW_DATA_DIV;
+ *
+ *	temp = readl(desc->base + REG_TSEN_LITE_TEMP_THRESH);
+ *	temp &= ~0xffff0000;
+ *	temp |= (over_thrsh << TSEN_HIGH_THRESH_OFFSET);
+ *	writel(temp, desc->base + REG_TSEN_LITE_TEMP_THRESH);
+ *
+ *	if (low < 0)
+ *		under_thrsh = 0;
+ *
+ *	under_thrsh /= 1000;
+ *	under_thrsh += desc->temp_offset;
+ *	under_thrsh *= TEMP_RAW_DATA_DIV;
+ *	temp = readl(desc->base + REG_TSEN_LITE_TEMP_THRESH);
+ *	temp &= ~0xffff;
+ *	temp |= (under_thrsh << TSEN_LOW_THRESH_OFFSET);
+ *	writel(temp, desc->base + REG_TSEN_LITE_TEMP_THRESH);
+ *
+ *	return 0;
+ * }
+ */
+static const struct thermal_zone_device_ops k3_of_thermal_ops = {
+	.get_temp = k3_thermal_get_temp,
+	/* .set_trips = k3_thermal_set_trips, */
+};
+
+/**
+ * static irqreturn_t k3_thermal_irq_thread(int irq, void *data)
+ * {
+ *	struct k3_thermal_sensor_desc *desc = (struct k3_thermal_sensor_desc *)data;
+ *
+ *	thermal_zone_device_update(desc->tzd, THERMAL_EVENT_UNSPECIFIED);
+ *
+ *	return IRQ_HANDLED;
+ * }
+ */
+
+static int k3_thermal_probe(struct platform_device *pdev)
+{
+	int ret, i;
+	struct resource *res;
+	struct k3_thermal_sensor *s;
+	struct device *dev = &pdev->dev;
+
+	s = devm_kzalloc(dev, sizeof(*s), GFP_KERNEL);
+	if (!s)
+		return -ENOMEM;
+
+	s->dev = dev;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	s->base = devm_ioremap_resource(dev, res);
+	if (IS_ERR(s->base))
+		return PTR_ERR(s->base);
+
+	ret = of_property_read_u32(dev->of_node, "temperature_offset", &s->temp_offset);
+	if (ret) {
+		dev_err(dev, "Get temperature_offset failed\n");
+		return -EINVAL;
+	}
+
+/**
+ *	s->irq = platform_get_irq(pdev, 0);
+ *	if (s->irq < 0) {
+ *		dev_err(dev, "failed to get irq number\n");
+ *		return -EINVAL;
+ *	}
+ */
+	s->resets = devm_reset_control_get_optional(&pdev->dev, NULL);
+	if (IS_ERR(s->resets))
+		return PTR_ERR(s->resets);
+
+        reset_control_deassert(s->resets);
+
+	s->fclk = devm_clk_get(dev, "func");
+	if (IS_ERR(s->fclk))
+		return PTR_ERR(s->fclk);
+
+	clk_prepare_enable(s->fclk);
+
+	s->bclk = devm_clk_get(dev, "bus");
+	if (IS_ERR(s->bclk))
+		return PTR_ERR(s->bclk);
+
+	clk_prepare_enable(s->bclk);
+
+	s->sdesc = (struct k3_thermal_sensor_desc *)devm_kzalloc(dev,
+			sizeof(struct k3_thermal_sensor_desc) * MAX_SENSOR_NUMBER,
+			GFP_KERNEL);
+
+	platform_set_drvdata(pdev, s);
+
+	/* initialize the sensors */
+	ret = init_sensors(pdev);
+
+	/* then register the thermal zone */
+	for (i = s->sr[0]; i <= s->sr[1]; ++i) {
+		s->sdesc[i].base = s->base;
+		s->sdesc[i].index = i;
+		s->sdesc[i].temp_offset = s->temp_offset;
+		s->sdesc[i].tzd = devm_thermal_of_zone_register(dev,
+				i, s->sdesc + i, &k3_of_thermal_ops);
+		if (IS_ERR(s->sdesc[i].tzd)) {
+			ret = PTR_ERR(s->sdesc[i].tzd);
+			dev_err(dev, "faild to register sensor id %d: %d\n",
+					i, ret);
+			return ret;
+		}
+
+		/* register the thermal sensor to hwmon */
+		if (devm_thermal_add_hwmon_sysfs(dev, s->sdesc[i].tzd))
+			dev_warn(dev, "Failed to add hwmon sysfs attributes\n");
+	}
+
+	/* enable the sensor interrupt & using auto mode */
+	enable_sensors(pdev);
+
+	return 0;
+}
+
+static void k3_thermal_remove(struct platform_device *pdev)
+{
+	int i;
+	struct k3_thermal_sensor *s = platform_get_drvdata(pdev);
+
+	/* disable the clk */
+	clk_disable_unprepare(s->fclk);
+	clk_disable_unprepare(s->bclk);
+	reset_control_assert(s->resets);
+
+	for (i = s->sr[0]; i <= s->sr[1]; ++i)
+		devm_thermal_of_zone_unregister(&pdev->dev, s->sdesc[i].tzd);
+
+	return;
+}
+
+static const struct of_device_id of_k3_thermal_match[] = {
+	{
+		.compatible = "spacemit,k3-tsensor",
+	},
+	{ /* end */ }
+};
+
+MODULE_DEVICE_TABLE(of, of_k3_thermal_match);
+
+static struct platform_driver k3_thermal_driver = {
+	.driver = {
+		.name		= "k3_thermal",
+		.of_match_table = of_k3_thermal_match,
+	},
+	.probe	= k3_thermal_probe,
+	.remove	= k3_thermal_remove,
+};
+
+module_platform_driver(k3_thermal_driver);
---
+++ drivers/thermal/k3-thermal.h
@@ -0,0 +1,51 @@
+#ifndef __k3_THERMAL_H__
+#define __k3_THERMAL_H__
+
+#define BITS(_start, _end) ((BIT(_end) - BIT(_start)) + BIT(_end))
+
+#define MAX_SENSOR_NUMBER		8
+#define TEMPERATURE_OFFSET		278
+
+#define REG_TSEN_LITE_CFG		0x00
+#define REG_TSEN_LITE_CFG2		0x04
+#define REG_TSEN_LITE_INT_ENB		0x08
+#define REG_TSEN_LITE_INT_ST		0x0C
+#define REG_TSEN_LITE_INT_CLR		0x10
+#define REG_TSEN_REBOOT_THRESH		0x14
+#define REG_TSEN_LITE_TEMP_DATA		0x18
+#define REG_TSEN_LITE_VREF_EFFUSE	0x1C
+#define REG_TSEN_LITE_TEMP_THRESH	0x20
+#define REG_TSEN_LITE_ECO_INTVL		0x24
+#define REG_TSEN_LITE_LP_FIX_BYPASS	0x28
+
+#define BITS_D_CK_DIV_SEL		BITS(2, 3)
+#define BITS_TEMP_DATA			BITS(0, 11)
+#define BITS_CK_DIV_SEL_DIV4		(0 << 2)
+#define BIT_TSEN_EN			BIT(0)
+
+#define TSEN_HIGH_THRESH_OFFSET		(0)
+#define TSEN_LOW_THRESH_OFFSET		(16)
+
+
+#define TEMP_RAW_DATA_DIV		(8)
+
+struct k3_thermal_sensor_desc {
+	void __iomem *base;
+	int temp_offset;
+	int index;
+	struct thermal_zone_device *tzd;
+};
+
+struct k3_thermal_sensor {
+	int irq;
+	int temp_offset;
+	void __iomem *base;
+	struct clk *fclk, *bclk;
+	struct reset_control *resets;
+	struct device *dev;
+	/* sensor range */
+	unsigned int sr[2];
+	struct k3_thermal_sensor_desc *sdesc;
+};
+
+#endif
