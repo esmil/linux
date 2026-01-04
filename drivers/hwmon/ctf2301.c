26e87af94be2446708f4fb1ca8775bdaf5ed3a71
Zexi Guo <zexi.guo@spacemit.com>
2026-01-04 11:50:20 +0800

    hwmon: (ctf2301): fix wrong update_pwm()
    don't need to map_val.
    
    Change-Id: I3cfc4c8e98826efd5d74dd9537bd414f15f8ff7c
    Signed-off-by: Zexi Guo <zexi.guo@spacemit.com>

--- drivers/hwmon/ctf2301.c
+++ drivers/hwmon/ctf2301.c
@@ -132,13 +132,9 @@ static int ctf2301_read_fan(struct device *dev, u32 attr, long *val)
 
 static int ctf2301_update_pwm(struct ctf2301 *data, long val)
 {
-	int map_val;
+	val = clamp_val(val, 0, 255);
 
-	clamp_val(val, 0, 255);
-
-	map_val = (val * data->pwm_freq_code * 2) / 255;
-
-	return regmap_write(data->regmap, CTF2301_PWM_VALUE, map_val);
+	return regmap_write(data->regmap, CTF2301_PWM_VALUE, val);
 }
 
 static int ctf2301_write_pwm(struct device *dev, u32 attr, long val)
